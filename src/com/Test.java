22a2047cc44fea4066a995f0d2e102e88a3ea778
hp <hp@10.80.2.177>
2017-05-02 16:51:56 +0800

    first edit

---
+++ src/com/Test.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Test {
+
+	public static <T> void printArray(T[] inputArray) {
+		for (T element : inputArray) {
+			System.out.printf("%s ", element);
+		}
+	}
+
+	public static void main(String args[]) {
+		Integer[] intArray = { 1, 2, 3, 4, 5 };
+		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+		Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+		printArray(intArray);
+		printArray(doubleArray);
+		printArray(charArray);
+	}
+}
